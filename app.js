d572e610990deab441acaf088650aa5397c7e38f
Ross Gray <rossgray@TechEd-0008.techeducators.co.uk>
2024-01-12 15:23:10 +0000

    First go

---
+++ app.js
@@ -0,0 +1,10 @@
+const agreeButton = document.getElementById("agree");
+const disagreeButton = document.getElementById("disagree");
+
+agreeButton.addEventListener("click", function () {
+  alert("Mmmm cookies my favourite!");
+});
+
+disagreeButton.addEventListener("click", function () {
+  alert(`Guess I'll starve then...`);
+});
